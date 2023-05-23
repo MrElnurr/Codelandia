264658966b22434bb2de93dda9f1f40b129cd2e7
MrElnurr <125906035+MrElnurr@users.noreply.github.com>
2023-05-23 20:00:54 +0400

    Update 23.05task mapsiz.dart

--- 23.05task mapsiz.dart
+++ 23.05task mapsiz.dart
@@ -23,3 +23,31 @@ void main() {
 
   print('Secdiyiniz Markanin sayi: $say');
 }
+
+
+//mapli
+
+void main() {
+  List<String> list = [
+    'ASUS',
+    'Asus',
+    'asus',
+    'HP',
+    'Hp',
+    'hp',
+    'MSI',
+    'msi',
+    'Msi'
+  ];
+
+  Map<String, int> mapping = {};
+
+  for (String produkt in list) {
+    String lowerCase = produkt.toLowerCase();
+    if (lowerCase == 'asus') {
+      mapping[lowerCase] = (mapping[lowerCase] ?? 0) + 1;
+    }
+  }
+
+  print(mapping);
+}
