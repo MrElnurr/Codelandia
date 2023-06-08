9a5a9ef89594299011c2fcfe2241691d7385896f
MrElnurr <125906035+MrElnurr@users.noreply.github.com>
2023-06-08 21:46:40 +0400

    Create praktiki.dart

---
+++ praktiki.dart
@@ -0,0 +1,31 @@
+class Car {
+  String minik = '';
+  String qruzovoy = '';
+  void showCar() {
+    print("secdiyiniz mashin tipi $minik");
+  }
+}
+
+class Model {
+  String model = '';
+}
+
+class MinikMashin extends Model {
+  String markasi = "";
+  void showMinikMashin() {
+    print("secdiyiniz mashinin modeli ve markasi $model $markasi");
+  }
+}
+
+void main(List<String> args) {
+  Car car = Car();
+  car.minik = "Minik";
+  car.showCar();
+  MinikMashin mashin = MinikMashin();
+  mashin.markasi = "M3";
+  mashin.model = "BMW";
+  mashin.showMinikMashin();
+
+  print(
+      "Secdiyiniz Mashin Tipi : ${car.minik}, Modeli ${mashin.model}, Markasi, ${mashin.markasi}");
+}
